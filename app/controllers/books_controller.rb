7ac9dfa9c1eaac88d90b0396e1e6abb4e13fd7d3
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-01-12 09:21:44 +0000

    全てのアクションとりあえず定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,10 @@
 class BooksController < ApplicationController
-  def index
+  def index#一覧ページ
+  
+    #一覧ページでは全てのデータを表示するから複数形の変数にしとく
+    #一覧ページに飛ぶ事で､すべてのデータが取得される  
+    @books = Book.all
+  
     #入力フォーム
     @book = Book.new
   end
@@ -7,26 +12,58 @@ class BooksController < ApplicationController
   def create#入力フォームのデータを保存する
   
     #1.ﾌｫｰﾑに入力されたデータを受け取る
-    #Book.newはフォームに入力されたデータを表している
-    #@book変数が入力ﾌｫｰﾑから､入力したﾃﾞｰﾀに更新された
-    @book = Book.new(book_params)
+    @book = Book.new(book_params)#Book.newはフォームに入力されたデータを表している。このｱｸｼｮﾝ内での@book変数は､入力ﾌｫﾑｰで､入力したﾃﾞｰﾀの事
     
-    #2.ﾌｫｰﾑに入力したデータをdbに保存する(bookにデータ格納されてる)
-    @book.save
+    @book.save#2.ﾌｫｰﾑで入力したデータをdbに保存する(bookにデータ格納されてる)
     
-    #詳細ページへリダイレクト
+    #3.book_pathで詳細ページへリダイレクト
     #@book.idでid番目(その時)にﾌｫｰﾑ入力されたﾃﾞｰﾀの詳細ページへ飛ぶ
     redirect_to book_path(@book.id)
   end
   
+  def destroy#一覧ページでデータを削除する時に使われる
+    #bookはBookモデルから削除するidのデータを探す変数
+    #削除ボタン押したら削除したいidのﾃﾞｰﾀを取得してくれる
+    book = Book.find(params[:id])
+    
+    #上で探してきたデータを削除
+    book.destroy
+    
+    #一覧ページを再表示
+    render :index
+  end
+ 
+  
   def show#詳細ページ
     #@bookにはcreateアクションの時にdbに保存したデータが入ってる
-    #@book変数が、特定idのﾃﾞｰﾀを探すという変数に更新された
+    #このｱｸｼｮﾝ内における@bookは特定idのﾃﾞｰﾀをBookモデルから探すという変数
     @book = Book.find(params[:id])
   end
 
-  def edit
+
+
+  def edit#編集ページ
+   #このｱｸｼｮﾝ内における@bookは特定idのﾃﾞｰﾀをBookモデルから探すという変数
+   #編集ボタンを押すと､編集したいidのﾃﾞｰﾀをﾓﾃﾞﾙ経由で取得する
+    @book = Book.find(params[:id])
   end
+  
+  def update#編集ページでﾃﾞｰﾀを更新する時に使われる
+  
+    #このbookは、Bookモデルから編集するidのデータを探す変数
+    #更新ﾎﾞﾀﾝ押したら､更新したいidのﾃﾞｰﾀを取得してくれる
+    book = Book.find(params[id])
+    
+    #上で探してきたデータを更新する
+    book.update(list_params)
+    
+    #book_pathで詳細ページへ飛ぶという意味
+    #book.idで上で取得したデータと同じidを指定している
+    redirect_to book_path(book.id)
+  end
+  
+  
+  
   
   private
   #ストロングパラメータ
