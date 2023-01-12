d17164ada8acf6a8ff52125ddcf168753c73dc78
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-01-12 06:42:56 +0000

    update 詳細ページにリダイレクトできるようになった Add showアクション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,21 +4,25 @@ class BooksController < ApplicationController
     @book = Book.new
   end
   
-  def create#入力フォームのデータを保存する(投稿ボタンを押す時点で)
+  def create#入力フォームのデータを保存する
+  
     #1.ﾌｫｰﾑに入力されたデータを受け取る
     #Book.newはフォームに入力されたデータを表している
-    #(Bookモデルのtitleとbodyの情報に絞り込み)
-    book = Book.new(book_params)
+    #@book変数が入力ﾌｫｰﾑから､入力したﾃﾞｰﾀに更新された
+    @book = Book.new(book_params)
     
     #2.ﾌｫｰﾑに入力したデータをdbに保存する(bookにデータ格納されてる)
-    book.save
+    @book.save
     
     #詳細ページへリダイレクト
     #@book.idでid番目(その時)にﾌｫｰﾑ入力されたﾃﾞｰﾀの詳細ページへ飛ぶ
-    redirect_to book_path
+    redirect_to book_path(@book.id)
   end
-
-  def show
+  
+  def show#詳細ページ
+    #@bookにはcreateアクションの時にdbに保存したデータが入ってる
+    #@book変数が、特定idのﾃﾞｰﾀを探すという変数に更新された
+    @book = Book.find(params[:id])
   end
 
   def edit
